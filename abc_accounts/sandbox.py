4f3cbe1e1d2b83c23c1ad709aa9a8108ad426ef8
Don Johnson <don.johnson@aofl.com>
2018-10-09 22:38:26 -0700

    refactored to limit requests to api , and avoid a rate abuse limit

--- abc_accounts/sandbox.py
+++ abc_accounts/sandbox.py
@@ -5,44 +5,48 @@ import json
 import requests
 import time
 
+TARGET_URL = "https://www.abcmouse.com"
+
+
+def account_create():
+    domain = TARGET_URL
+    parent = Account(domain)
+    parent.add_child("Student 1", 2, "M")
+    parent.add_child("Student 2", 7, "F")
+    register.new('parent',parent)
+    return [parent.email, parent.password]
+
+def account_record(uname, password):
+    login_endpoint = "/apis/mws/0.3/json/Member/Login/init"
+    auth = [uname, password]
+    args = dict(arguments=json.dumps(auth))
+    req = requests.post(TARGET_URL + login_endpoint, data=args)
+    res = req.json()
+    return res
+
+
+
+account = account_create()
+uname = account[0]
+password = account[1]
+print(uname, password)
+master_record = account_record(uname, password)
+
+print(master_record)
+
 
 class OnboardingAPITest(unittest.TestCase):
 
-    def setUp(self):
-        self.login_endpoint = "/apis/mws/0.3/json/Member/Login/init"
-        self.billing_endpoint = "/apis/mws/0.3/json/Parent/GetBillingInfo/init"
-        self.domain = "https://cs.qtest.abcmouse.com"
-        self.parent = Account(host=self.domain)
-        self.parent.add_child("Student 1", 2, "M")
-        self.parent.add_child("Student 2", 7, "F")
-        time.sleep(10)
-        register.new('parent', self.parent)
-        self.user_name = self.parent.email
-        self.password = self.parent.password
-
-
     def test_end_end_account_creation_paytech(self):
-        creds = [self.user_name, self.password]
-        args = dict(arguments=json.dumps(creds))
-        req = requests.post(self.domain + self.login_endpoint, data=args)
-        res = req.json()
-        account_login_action = res['success']
+        account_login_action = master_record['success']
         self.assertEqual(account_login_action, "TRUE")
 
     def test_master_account_id_is_of_type_integer(self):
-        creds = [self.user_name, self.password]
-        args = dict(arguments=json.dumps(creds))
-        req = requests.post(self.domain + self.login_endpoint, data=args)
-        res = req.json()
-        account_id = res['payload', 'master_account_member_id']
-        self.assertTrue(type(account_id), type(int))
+        account_id = master_record['payload', 'master_account_member_id']
+        self.assertTrue(type(account_id), int)
 
     def test_master_account_type_group_is_set_to_parent(self):
-        creds = [self.user_name, self.password]
-        args = dict(arguments=json.dumps(creds))
-        req = requests.post(self.domain + self.login_endpoint, data=args)
-        res = req.json()
-        mast_account_type = res['payload']['master_account_account_type_group']
+        mast_account_type = master_record['payload']['master_account_account_type_group']
         self.assertEqual(mast_account_type, "parent")
 
 
